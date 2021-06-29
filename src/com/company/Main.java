2371ec7495d7ce22daaf129f9a8062d39a2b2a4e
ChecoChecoGH <as.ignasi97@gmail.com>
2021-06-29 17:25:24 +0200

    proyecto resumen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //constante
         final int CONSTANTE = 0;
@@ -13,25 +18,121 @@ public class Main {
         String cadena = "";
         boolean booleano = true;
 
-        System.out.println("Hola buenas");
+        BufferedReader tc=new BufferedReader(new InputStreamReader(System.in));
 
-        if(x == 0){
-            System.out.println("!");
-        }else if(x == 1){
-            System.out.println("ADS");
+        //cadena = tc.readLine();
+
+
+
+        //ARRAY
+        int [] array1 = new int[3];
+
+       // int y = 5;
+/*
+        for(x = 0; x < 6; x++){
+            array1[x] = y;
+        }
+*/
+        for(x = 0; x < 3; x++){
+            array1[x] = Integer.parseInt(tc.readLine());
         }
 
+        for(x = 0; x < 3; x++){
+            System.out.println(array1[x]);
+        }
+
+
+
+
+        /*
+
+
+        System.out.println("Hola buenas");
+
+        System.out.print(cadena);
+
+
+        if(x >= 0){
+            System.out.println("suuuu!");
+        }else if(x >= 1){
+                System.out.println("hola");
+            }else{
+                System.out.println("adios");
+            }
+
+
+
+
+        //while 0 - a infinito
         while(x == 0){
             System.out.println("Hola");
         }
 
+
+        //do while 1 - infinito
         do{
             System.out.println("Hola");
         }while(x == 0);
 
-        for(int i = 0; i <= 10; i++){
-            System.out.println(i);
+
+        //for se el numero de veces
+        for(int vi = 0; vi <= 10; vi++){//incremento de 1
+            System.out.println("adsasd" + vi);
+
         }
+
+*/
+
+        /*
+        operaciones:
+            + suma
+            - resta
+            / division
+             * multiplicacion
+             % resto
+             ++ incremento x = x + 1
+             -- decremento x = x - 1
+
+        de relacion (dentro de las condiciones)
+            <
+            <=
+            >
+            >=
+            == comparar si son iguales
+            != comparar si son distintos
+
+        condicionales
+            &&  y
+            ||  o
+            !   no
+
+        asignación
+            = asignación simple
+        (ampliacion, todavía no)
+            +=
+            -=
+            *=
+            /=
+            %=
+
+
+
+        * */
+
+
+
+
+
+//        int y = 0;
+
+  //      y = 5;
+
+        //escribir la variable y
+        //escribir dos veces la variable y
+        //escribir 20 veces la variable y
+
+
+
 
     }
 }
